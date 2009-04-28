4c4924ed9acd4a927295a13475f0943f5bfbffdd
Brad Beattie <bradbeattie@gmail.com>
2009-04-28 11:39:06 -0700

    Incrementing the version number

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_schedules do
 	name 'Redmine Schedules plugin'
 	author 'Brad Beattie'
 	description 'This plugin provides instances of Redmine a method with which to allocate users to projects and to track this allocation over time. It does so by creating daily time estimates of hours worked per project per user.'
-	version '0.3.2'
+	version '0.4.0'
   
 	project_module :schedule_module do
 		permission :view_schedules,  {:schedules => [:index]}, :require => :member
